d97a64e1ffcf3a3175d3feec5a1ce9dfa8e01754
hisato tanaka <hisato.tanaka@gmail.com>
2014-03-17 12:54:07 +0900

    tax 1.08 support

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -1,15 +1,28 @@
+class Integer
+  def to_currency()
+    self.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\1,').reverse
+  end
+end
 module MenuHelper
+  THEDAY20140401 = Time.new(2014,4,1).freeze
   def digit(val)
-    yval =  val.to_s + "円(税込)"
-    ret = image_tag64 "menu/digit/y.gif",:alt => yval
-    idx = 0
-    val.to_s.each_char{ |s|
-      ret += image_tag64 "menu/digit/#{s}.gif" ,:alt => yval
-      ret += image_tag64 "menu/digit/cm.gif",:alt => yval if val >= 1000 and idx == 0
-      idx+= 1
-    }
-    ret += image_tag64 "menu/digit/t.gif",:alt => yval
-    return ret;
+    #税込価格
+    wtax = val.to_currency
+    #税抜き価格
+    tax = Time.current() >= THEDAY20140401 ? 0.08 : 0.05
+    wotax = (val / (1+tax)).ceil.to_currency
+    alt = "税込価格:#{wtax}円 税抜本体価格:#{wotax}円"
+    wtax_img = wtax.split('').map do |d|
+      image_tag64 "menu/digit/#{d}.gif" ,:alt => alt
+    end
+    #円 税込みの追加
+    wtax_img.push image_tag64 "menu/digit/t.gif",:alt => alt
+    #税抜の追加
+    wotax_img = wotax.split('').map do |d|
+      image_tag64 "menu/digit/#{d}.gif" ,:alt => alt
+    end
+    wotax_img.push image_tag64 "menu/digit/t2.gif",:alt => alt
+    return raw (wtax_img + wotax_img).join('')
   end
 
 end
