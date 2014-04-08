f28d5ad2ffb6d1a3145c7d922e276340af321a5a
hisato tanaka <hisato.tanaka@gmail.com>
2014-04-08 10:55:22 +0900

    precompile add

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,8 +1,8 @@
 Chef::Log.info("Running deploy/before_migrate.rb")
 env = node[:deploy][:oldweb][:rails_env]
 current_release = release_path
-# execute "rake assets:precompile" do
-#   cwd current_release
-#   command "bundle exec rake assets:precompile"
-#   environment "RAILS_ENV" => env
-# end
+execute "rake assets:precompile" do
+  cwd current_release
+  command "bundle exec rake assets:precompile"
+  environment "RAILS_ENV" => env
+end
