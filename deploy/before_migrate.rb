68de56d5a5b40b46fed5129bb918b4dd080d9000
hisato tanaka <hisato.tanaka@gmail.com>
2014-04-17 17:48:54 +0900

    precompile disabled

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,11 +1,11 @@
-Chef::Log.info("Running deploy/before_migrate.rb")
-env = node[:deploy][:oldweb][:rails_env]
-current_release = release_path
-execute "rake assets:precompile" do
-  cwd current_release
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => env
-end
+# Chef::Log.info("Running deploy/before_migrate.rb")
+# env = node[:deploy][:oldweb][:rails_env]
+# current_release = release_path
+# execute "rake assets:precompile" do
+#   cwd current_release
+#   command "bundle exec rake assets:precompile"
+#   environment "RAILS_ENV" => env
+# end
 
 #environment "SES_KEY" => node[:deploy][:oldweb][:ses_key]
 #environment "SES_SECRET" => node[:deploy][:oldweb][:ses_secret]
