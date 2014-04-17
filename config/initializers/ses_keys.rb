bf7e7b4cd06a1410947b2c0df4f2715af70e1033
hisato tanaka <hisato.tanaka@gmail.com>
2014-04-17 18:36:46 +0900

    tmp ses disabled

--- config/initializers/ses_keys.rb
+++ config/initializers/ses_keys.rb
@@ -1,3 +1,3 @@
-SES = AWS::SES::Base.new(access_key_id: ENV["SES_KEY"],secret_access_key: ENV["SES_SECRET"])
+#SES = AWS::SES::Base.new(access_key_id: ENV["SES_KEY"],secret_access_key: ENV["SES_SECRET"])
 
-#SES.send_email(to:'hisato.tanaka@gmail.com',source:'info@yamaokaya.com',subject:'test',body:'test')
+
