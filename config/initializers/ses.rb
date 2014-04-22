ac8451e21f436b98471a95cdad6fc374021bcc8f
hisato tanaka <hisato.tanaka@gmail.com>
2014-04-22 19:57:35 +0900

    SES!!

--- config/initializers/ses.rb
+++ config/initializers/ses.rb
@@ -1,1 +1,1 @@
-::SES = AWS::SES::Base.new(access_key_id: ENV["SES_KEY"],secret_access_key: ENV["SES_SECRET"])
+SES = AWS::SES::Base.new(access_key_id: ENV["SES_KEY"],secret_access_key: ENV["SES_SECRET"])
