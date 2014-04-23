29395ddc7d0e4adbd60f810bf28d13e9923ffab9
hisato tanaka <hisato.tanaka@gmail.com>
2014-04-23 09:37:30 +0900

    precom

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,9 +1,9 @@
-# Chef::Log.info("Running deploy/before_migrate.rb")
-# env = node[:deploy][:oldweb][:rails_env]
-# current_release = release_path
-# execute "rake assets:precompile" do
-#   cwd current_release
-#   command "bundle exec rake assets:precompile"
-#   environment "RAILS_ENV" => env
-# end
+Chef::Log.info("Running deploy/before_migrate.rb")
+env = node[:deploy][:oldweb][:rails_env]
+current_release = release_path
+execute "rake assets:precompile" do
+  cwd current_release
+  command "bundle exec rake assets:precompile"
+  environment "RAILS_ENV" => env
+end
 
