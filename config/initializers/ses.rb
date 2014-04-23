18bb0ef99a11c7cc463e540e3a0458f78e75d468
hisato tanaka <hisato.tanaka@gmail.com>
2014-04-23 11:50:12 +0900

    env var2

--- config/initializers/ses.rb
+++ config/initializers/ses.rb
@@ -1,1 +1,3 @@
-OLDWEB::SES = AWS::SES::Base.new(access_key_id: ENV['SES_KEY'],secret_access_key: ENV['SES_SECRET'])
+module OLDWEB
+  SES = AWS::SES::Base.new(access_key_id: ENV['SES_KEY'],secret_access_key: ENV['SES_SECRET'])
+end
