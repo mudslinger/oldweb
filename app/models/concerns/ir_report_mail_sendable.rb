7ee091b33d1371c26df14170a080fa27ea4508b5
hisato tanaka <hisato.tanaka@gmail.com>
2014-04-23 10:39:35 +0900

    precom3

--- app/models/concerns/ir_report_mail_sendable.rb
+++ app/models/concerns/ir_report_mail_sendable.rb
@@ -6,7 +6,9 @@ module IrReportMailSendable
   end
 
   def report
-    ::SES.send_email(
+    ses = AWS::SES::Base.new(access_key_id: ENV["SES_KEY"],secret_access_key: ENV["SES_SECRET"])
+
+    ses.send_email(
       :to        => 'ir@yamaokaya.com',
       :source    => 'info@yamaokaya.com',
       :subject   => "IR問い合わせメール(#{self.id})",
