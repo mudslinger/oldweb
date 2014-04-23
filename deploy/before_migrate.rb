cdb527da32a66ac124b05f36595fe3a2a56a6b8b
hisato tanaka <hisato.tanaka@gmail.com>
2014-04-23 10:53:53 +0900

    precom5

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,13 +1,5 @@
 Chef::Log.info("Running deploy/before_migrate.rb")
 
-# map the environment_variables node to ENV
-node[:deploy].each do |application, deploy|
-  deploy[:environment_variables].each do |key, value|
-    Chef::Log.info("Setting ENV[#{key}] to #{value}")
-    ENV[key] = value
-  end if deploy[:environment_variables]
-end
-
 env = node[:deploy][:oldweb][:rails_env]
 current_release = release_path
 execute "rake assets:precompile" do
