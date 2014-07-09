596f40570a0da37bb921893bbbcb60d50fbe8f7c
hisato tanaka <hisato.tanaka@gmail.com>
2014-07-09 09:55:55 +0900

    xx

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -9,11 +9,11 @@ node[:deploy].each do |application, deploy|
   end if deploy[:environment_variables]
 end
 
-env = node[:deploy][:oldweb][:rails_env]
-current_release = release_path
-execute "rake assets:precompile" do
-  cwd current_release
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => env
-end
+# env = node[:deploy][:oldweb][:rails_env]
+# current_release = release_path
+# execute "rake assets:precompile" do
+#   cwd current_release
+#   command "bundle exec rake assets:precompile"
+#   environment "RAILS_ENV" => env
+# end
 
